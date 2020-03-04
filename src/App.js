0aa2fdb5d7c628b07ba24ed6739f4f68ffe431fa
Saurabh Thakur <saurabh.thakur@appdirect.com>
2020-03-05 00:57:38 +0530

    2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dude !
+          Dude ! Next featutre 
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Change branch 
+          Learn React
         </a>
       </header>
     </div>
